5ab373470b1c6c668ffcd11515838c88c261474b
Dominik <wrobel.dominik.world@gmail.com>
2018-12-23 22:00:57 +0100

    JavaProject

--- elearning/src/controllers/PlatformController.java
+++ elearning/src/controllers/PlatformController.java
@@ -2,12 +2,15 @@ package controllers;
 
 import database.DatabaseService;
 import entities.Quizz;
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.util.ArrayList;
 
@@ -35,18 +38,17 @@ public class PlatformController {
         DatabaseService databaseService = DatabaseService.getInstance();
 
         ArrayList<Quizz> usersQuizzes = databaseService.getUsersQuizzes(primaryStage.getTitle());
-        for(Quizz q : usersQuizzes){
-            System.out.println(q.getQuizId() + q.getQuizName() + q.getUsername());
-        }
 
-        for(int i = 0 ; i < 10 ; i++){
+        for(int i = 0 ; i < usersQuizzes.size() ; i++){
+
+            Quizz q = usersQuizzes.get(i);
 
             if(i % 5 == 0 && i != 0){
                 yPosition = yPosition + yPositionChange;
                 xPosition = initialX;
             }
 
-            Button button1 = new Button("Quiz1");
+            Button button1 = new Button(q.getQuizName());
             button1.setMinWidth(buttonWidth);
             button1.setMinHeight(buttonHeight);
             button1.setStyle("-fx-background-color: #1d1d1d; -fx-font-family: Arial; -fx-font-size: 25px;" +
@@ -58,6 +60,16 @@ public class PlatformController {
 
             xPosition = xPosition + xPositionChange;
 
+
+            TranslateTransition tt =
+                    new TranslateTransition(Duration.seconds(5), button1);
+
+            tt.setFromX( 0 );
+            tt.setToX( button1.getTranslateX() );
+            tt.setToY( button1.getTranslateY() );
+            tt.play();
+
+            System.out.println(q.getQuizId() + q.getQuizName() + q.getUsername());
         }
 
 
