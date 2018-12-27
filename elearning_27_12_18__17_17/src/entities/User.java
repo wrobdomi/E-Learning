2ea51c9d4809c5097ae44fc6fc404c9803066486
Dominik <wrobel.dominik.world@gmail.com>
2018-12-27 17:19:05 +0100

    Adding removing quizes

---
+++ elearning_27_12_18__17_17/src/entities/User.java
@@ -0,0 +1,15 @@
+package entities;
+
+public class User {
+
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String un) {
+        this.username = un;
+    }
+
+}
