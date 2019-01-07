6644eabee37d4b730c79cf21851777f6af3d3bf1
Dominik <wrobel.dominik.world@gmail.com>
2019-01-07 22:54:48 +0100

    E-Learning

--- elearning_24_12_18__21_31/src/enums/QuizOptions.java
+++
@@ -1,5 +0,0 @@
-package enums;
-
-public enum QuizOptions {
-    ADDQ, START
-}
--- elearning_27_12_18__17_17/src/test/testController.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-import javafx.scene.input.MouseEvent;
-
-public class testController {
-    public void testMethod(MouseEvent event) {
-        System.out.println("jakas metoda tutaj");
-    }
-}
